2171051cb22d6cedc01c226ae8f49e547e52a9bd
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-02-25 14:27:18 -0800

    cconfig

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# SSH client configuration must use the private key ~/.ssh/school
+# SSH client configuration must refuse to authenticate using a password
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}

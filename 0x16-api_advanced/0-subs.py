18d83dc5a4af20225518c331703b1a0e7e089200
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-04-09 04:24:43 -0700

    how many sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,11 +9,9 @@ import requests
 def number_of_subscribers(subreddit):
     """Return total number of subscribers subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
     headers = {
         "User-Agent": "ALX API student"
     }
-
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0

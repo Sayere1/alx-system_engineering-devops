da27f72aefe1c795a1164b14a9e3cb1a64bb3396
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-04-10 13:22:30 -0700

    debug 3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Apache 500 error fixed and automated using Puppet.
+
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
+}
